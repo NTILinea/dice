e2686f6e3d41b7274e2083e0428b7a9a26aaec14
NTILinea <linea.petersen@elev.ga.ntig.se>
2020-11-03 16:20:46 +0100

    start
    need alot of help

---
+++ js/app.js
@@ -0,0 +1,9 @@
+document.querySelector("button").addEventListener("click", function(){
+
+    /*document.getElementById("diceLog").innerHTML = alert ("test")*/
+})
+
+let nr = Math.floor ((Math.random()*10)+1)
+document.querySelector("p").src = nr
+
+//document.querySelector("img").src = "DiceRolls/" + nr + ".png"
