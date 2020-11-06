207bd58de117432b94a8022186dc825adbb3fab1
NTILinea <linea.petersen@elev.ga.ntig.se>
2020-11-06 13:29:18 +0100

    Function finished
    got the funtion working now. gotta fix the size of the dice now

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
-document.querySelector("button").addEventListener("click", nr)
+document.querySelector("button").addEventListener("click", showNr)
 
     /*document.getElementById("diceLog").innerHTML = alert ("test")*/
 
-function runThis() {
-    alert("Hello! I am an alert box!!");
+/*function runThis() {
+    alert("Hello! I am an alert box!!")*/
+
+
+
+function showNr() {
+    //document.querySelector("p").innerHTML = nr
+    let nr = Math.floor ((Math.random()*10)+1)
+    document.querySelector("img").src = "DiceRolls/" + nr + ".png"
+    console.log(nr)
 }
 
 
-let nr = Math.floor ((Math.random()*10)+1)
 
-document.querySelector("p").href = nr
-
-//document.querySelector("img").src = "DiceRolls/" + nr + ".png"
