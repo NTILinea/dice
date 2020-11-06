987ba4adcd59e0abb6133d81cbe4aa06e7e673d2
NTILinea <linea.petersen@elev.ga.ntig.se>
2020-11-06 13:00:13 +0100

    yeet

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
-document.querySelector("button").addEventListener("click", function(){
+document.querySelector("button").addEventListener("click", nr)
 
     /*document.getElementById("diceLog").innerHTML = alert ("test")*/
-})
+
+function runThis() {
+    alert("Hello! I am an alert box!!");
+}
+
 
 let nr = Math.floor ((Math.random()*10)+1)
-document.querySelector("p").src = nr
+
+document.querySelector("p").href = nr
 
 //document.querySelector("img").src = "DiceRolls/" + nr + ".png"
